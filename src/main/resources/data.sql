168325494f9c430c8d6fa19ddedb059966c53d23
9nh5 <koon6756@naver.com>
2026-07-12 22:19:41 +0900

    feat: 테이블 및 임시 데이터 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,73 @@
+INSERT INTO TB_CODE_GROUP(
+    GROUP_CODE,
+    GROUP_NAME,
+    DESCRIPTION,
+    CREATED_AT,
+    CREATED_BY
+) VALUES (
+          'BANK',
+          '은행코드',
+          '은행코드구분',
+          CURRENT_TIMESTAMP,
+          'ADMIN'
+         );
+
+INSERT INTO TB_COMMON_CODE(
+    GROUP_CODE,
+    COMMON_CODE,
+    CODE_NAME,
+    USE_YN,
+    SORT_ORDER,
+    DESCRIPTION,
+    CREATED_AT,
+    CREATED_BY
+) VALUES (
+          'BANK',
+          '001',
+          '우리은행',
+          'Y',
+          1,
+          '우리은행코드',
+          CURRENT_TIMESTAMP,
+          'ADMIN'
+         );
+
+INSERT INTO TB_COMMON_CODE(
+    GROUP_CODE,
+    COMMON_CODE,
+    CODE_NAME,
+    USE_YN,
+    SORT_ORDER,
+    DESCRIPTION,
+    CREATED_AT,
+    CREATED_BY
+) VALUES (
+             'BANK',
+             '002',
+             '국민은행',
+             'Y',
+             2,
+             '국민은행코드',
+             CURRENT_TIMESTAMP,
+             'ADMIN'
+         );
+
+INSERT INTO TB_COMMON_CODE(
+    GROUP_CODE,
+    COMMON_CODE,
+    CODE_NAME,
+    USE_YN,
+    SORT_ORDER,
+    DESCRIPTION,
+    CREATED_AT,
+    CREATED_BY
+) VALUES (
+             'BANK',
+             '003',
+             '신한은행',
+             'Y',
+             3,
+             '신한은행코드',
+             CURRENT_TIMESTAMP,
+             'ADMIN'
+         );
